a1cec28e41cb6e0d05ff6f380e943bf5420b50a2
b_kim82 <brandonkim0802@gmail.com>
2019-01-16 12:29:43 -0500

    ur cool 2

---
+++ Test.py
@@ -0,0 +1,51 @@
+from tkinter import *
+
+
+class Application(Frame):
+    def __init__(self,master):
+
+        super(Application, self).__init__(master)
+        self.grid()
+        self.bttn_clicks = 0
+        self.create_widget()
+        self.create_widgets2()
+
+
+    def create_widget(self):
+        photo = PhotoImage(file="peppermint11.png")
+        self.bttn = Button(self, image=photo)
+        self.photo = photo
+        self.bttn.place(x=20, y=20)
+        self.bttn.grid(row=0, column=0)
+
+
+    def create_widgets2(self):
+        self.bttn2 = Button(self)
+        self.bttn2["text"] = "Total Clicks: 0"
+        self.bttn2["command"] = self.update_count
+        self.bttn2.place(x=40, y=40)
+        self.bttn2.grid(row=0, column=3)
+
+
+
+    def update_count(self):
+        self.bttn_clicks += 1
+        self.bttn2["text"] = "Total Clicks: " + str(self.bttn_clicks)
+
+    # def callback():
+    #     print
+    #     "click!"
+
+    # def peppermint(master):
+    #     photo = PhotoImage(file="peppermint11.png")
+    #     b = Button(master, image=photo, text="Click!!", height=50, width=150, compound=LEFT)
+    #     b.place(x=20, y=20)
+
+def callback(self):
+    print("click!")
+
+root = Tk()
+root.title("Click Counter")
+root.geometry("600x400")
+app = Application(root)
+root.mainloop()
--- hello.py
+++ hello.py
@@ -37,34 +37,7 @@ canvas.pack(fill = BOTH, expand = True)
 photo = PhotoImage(file = './logo.gif')
 canvas.create_image(55, 55, image=photo)
 canvas.create_text(600, 155, text = 'Welcome', font = ('Helvetica', 72, 'bold'), justify = 'center', fill='blue')
-canvas.updimport Tkinter as tk
-button_flag = True
-def click():
-    """
-    respond to the button click
-    """
-    global button_flag
-    # toggle button colors as a test
-    if button_flag:
-        button1.config(bg="white")
-        button_flag = False
-    else:
-        button1.config(bg="green")
-        button_flag = True
-root = tk.Tk()
-# create a frame and pack it
-frame1 = tk.Frame(root)
-frame1.pack(side=tk.TOP, fill=tk.X)
-# pick a (small) image file you have in the working directory ...
-photo1 = tk.PhotoImage(file="Press1.gif")
-# create the image button, image is above (top) the optional text
-button1 = tk.Button(frame1, compound=tk.TOP, width=155, height=55, image=photo1,
-    text="optional text", bg='green', command=click)
-button1.pack(side=tk.LEFT, padx=2, pady=2)
-# save the button's image from garbage collection (needed?)
-button1.image = photo1
-# start the event loop
-root.mainloop()ate
+canvas.update
 
 # Make panewindow child of root
 panewindow = ttk.Panedwindow(root, orient = VERTICAL)
