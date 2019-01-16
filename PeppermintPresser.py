f67bef6ceb9848d986dff0932dfa86569b2f52aa
Joseph Tufano <josephtufano03@gmail.com>
2019-01-16 09:03:35 -0500

    Initial commit

---
+++ PeppermintPresser.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+class Peppermint(Frame):
+    def __init__(self, master):
+        super(Peppermint, self).__init__(master)
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets():
+        root = Tkinter()
+        '''Peppermint button'''
+        left = Frame(root, borderwidth=2, relief="solid")
+        right = Frame(root, borderwidth=2, relief="solid")
+        container = Frame(left, borderwidth=2, relief="solid")
+        box1 = Frame(right, borderwidth=2, relief="solid")
+        box2 = Frame(right, borderwidth=2, relief="solid")
+        self.Penguin = Button(row=1, )
+        self.polar_bears
+        self.christmas_tree_forest
+        self.present_machine
+        self.candy_cane_factory
+        self.Eskimo
+        self.iogloo
+        self.north_pole
+        self.ice_age
+        self.pluto
+
+
+
+
