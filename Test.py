e819d0a02d5707a47ca7f345ffbd55ffbc895cf1
b_kim82 <brandonkim0802@gmail.com>
2019-01-17 10:06:12 -0500

    ur cool 2

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import time
 
 class Application(Frame):
     def __init__(self,master):
@@ -9,29 +9,52 @@ class Application(Frame):
         self.bttn_clicks = 0
         self.create_widget()
         self.create_widgets2()
+        self.create_widgets3()
+        self.create_widgets4()
 
 
     def create_widget(self):
         photo = PhotoImage(file="peppermint11.png")
         self.bttn = Button(self, image=photo)
         self.photo = photo
-        self.bttn.place(x=20, y=20)
+        self.bttn.place(x=90, y=20)
         self.bttn.grid(row=0, column=0)
         self.bttn["command"] = self.update_count
 
 
     def create_widgets2(self):
         self.bttn2 = Button(self)
-        self.bttn2["text"] = "Total Clicks: 0"
+        self.bttn2["text"] = "Peppermints: 0"
 
         self.bttn2.place(x=40, y=40)
-        self.bttn2.grid(row=0, column=3)
+        self.bttn2.grid(row=8, column=90)
+
+    def create_widgets3(self):
+        self.bttn3 = Button(self)
+        self.bttn3["text"] = "Redo"
+        self.bttn3.grid(row=20, column= 90)
+        self.bttn3.place(x=40, y=60)
+        self.bttn3["command"] = self.reset
+
+    def create_widgets4(self):
+        self.bttn4 = Button(self)
+        self.bttn4["text"] = "Add"
+        self.bttn4.grid(row = 50, column = 90)
+        self.bttn4.place(x=60, y=79)
+        self.bttn4["command"] = self.add_cookies
 
 
 
     def update_count(self):
         self.bttn_clicks += 1
-        self.bttn2["text"] = "Total Clicks: " + str(self.bttn_clicks)
+        self.bttn2["text"] = "Peppermints: " + str(self.bttn_clicks)
+
+    def reset(self):
+        self.bttn_clicks = 0
+
+    def add_cookies(self):
+        self.bttn_clicks += 50
+
 
     # def callback():
     #     print
@@ -46,7 +69,7 @@ def callback(self):
     print("click!")
 
 root = Tk()
-root.title("Click Counter")
-root.geometry("600x400")
+root.title("Peppermint Clicker")
+root.geometry("999x990")
 app = Application(root)
 root.mainloop()
